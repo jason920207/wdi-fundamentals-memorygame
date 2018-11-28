b5e208248f300556caf5fff1c65f31071e905e2c
xiaojie zhang <jason920207@gmail.com>
2018-11-28 12:20:44 -0500

    create game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * @Author: xiaojiezhang
+ * @Date:   2018-11-28T11:46:08-05:00
+ * @Last modified by:   xiaojiezhang
+ * @Last modified time: 2018-11-28T12:19:26-05:00
+ */
+var cardOne="queen";
+var cardTwo="queen";
+var cardThree="king";
+var cardFour="king";
+console.log("User flipped " + cardOne);
