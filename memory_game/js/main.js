44bd2514b6f828a463a31d9b61c284251354399e
xiaojie zhang <jason920207@gmail.com>
2018-11-28 13:55:16 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,10 +2,20 @@
  * @Author: xiaojiezhang
  * @Date:   2018-11-28T11:46:08-05:00
  * @Last modified by:   xiaojiezhang
- * @Last modified time: 2018-11-28T12:19:26-05:00
+ * @Last modified time: 2018-11-28T13:51:57-05:00
  */
-var cardOne="queen";
-var cardTwo="queen";
-var cardThree="king";
-var cardFour="king";
-console.log("User flipped " + cardOne);
+var cards=["queen", "queen", "king", "king"];
+var cardsInPlay=[];
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay[0]===cardsInPlay[1]){
+  alert("You found a match!");
+}else{
+  alert("Sorry, try again.");
+}
