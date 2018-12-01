972108e6d8ce62f6791f83c75865fa6304847cfe
xiaojie zhang <jason920207@gmail.com>
2018-11-30 20:52:52 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,59 @@
  * @Author: xiaojiezhang
  * @Date:   2018-11-28T11:46:08-05:00
  * @Last modified by:   xiaojiezhang
- * @Last modified time: 2018-11-28T13:51:57-05:00
+ * @Last modified time: 2018-11-30T20:52:02-05:00
  */
-var cards=["queen", "queen", "king", "king"];
-var cardsInPlay=[];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
 
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardTwo);
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  }, {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  }, {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  }, {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }];
+var cardsInPlay = [];
 
-if(cardsInPlay[0]===cardsInPlay[1]){
-  alert("You found a match!");
-}else{
-  alert("Sorry, try again.");
-}
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+  }
+var flipCard = function() {
+    var cardId = this.getAttribute("data-id");
+	  var card = cards[cardId];
+	    this.setAttribute("src", card.cardImage);
+	    this.setAttribute("class", "clicked");
+	    console.log(this);
+	      cardsInPlay.push(card.rank);
+	        if (cardsInPlay.length === 2) {
+		        checkForMatch();
+	        }
+  }
+var createBoard =function() {
+    for(var i = 0; i < cards.length; i++) {
+      var cardElement = document.createElement("img");
+		      cardElement.setAttribute("src", "images/back.png");
+		      cardElement.setAttribute("data-id", i)
+		      cardElement.addEventListener("click", flipCard);
+		  var board = document.getElementById("game-board");
+		    board.appendChild(cardElement);
+    }
+  }
+
+createBoard();
+document.querySelector("button").addEventListener("click", resetGame);
